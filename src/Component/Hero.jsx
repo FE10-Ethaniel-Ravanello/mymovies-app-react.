8d5023ea779a80cbe055ba4686ab55a80440f16f
Aventom <ravaneloe@gmail.com>
2022-11-23 07:24:23 +0700

    Penambahan tampilan

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -4,13 +4,13 @@ import data from '../data'
 class Hero extends Component {
   render() {
     return (
-    <div className='max-w-[1640px] mx-auto p-4'>
-        <h2 className='text-center font-bold text-5xl mb-3'>Movie PLaying Now!!</h2>
+    <div className='max-w-screen mx-auto p-4'>
+        <h2 className='px-[120px] font-bold text-5xl mb-3'>Movie Playing Now!!</h2>
         <div className="carousel w-full">
             {data.map((item) => {
             return (
             <div id={item.id} className="carousel-item relative w-full bg-base-100 shadow-xl">
-                <img src={item.image} className="w-[1640px] h-[600px] object-cover" />
+                <img src={item.image} className="w-[1640px] h-[600px] object-cover mx-auto" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href={item.left} className="btn btn-circle">❮</a> 
                     <a href={item.right} className="btn btn-circle">❯</a>
--- src/Component/List.jsx
+++ src/Component/List.jsx
@@ -5,12 +5,34 @@ class List extends Component {
   render() {
     return (
       <div className='max-w-[1640px] mx-auto justify-between items-center p-4'>
-        <h2 className='font-bold text-2xl mb-4'>Like Movies</h2>
+        <h2 className='font-bold text-2xl md:text-3xl lg:text-4xl mb-2'>Top Like Movies</h2>
+        {/* Filter Row */}
+        <div className='max-w-[1640px] m-auto py-4'>
+            {/* Filter Series/Movies */}
+            <div>
+              <p className='font-bold text-gray pb-3'>Type</p>
+              <div>
+                <button className='bg-gray-400 rounded-full mb-3 py-1 px-3 font-bold mr-2' type='button'>Movies</button>
+                <button className='bg-gray-400 rounded-full mb-3 py-1 px-3 font-bold mx-2' type='button'>Series</button>
+              </div>
+            </div>
+            {/* Filter Genre */}
+            <div>
+              <p className='font-bold text-gray pb-3'>Genre</p>
+              <div>
+                <button className='bg-gray-400 rounded-full py-1 px-3 font-bold mr-2' type='button'>Action</button>
+                <button className='bg-gray-400 rounded-full py-1 px-3 font-bold mr-2' type='button'>Thriller</button>
+                <button className='bg-gray-400 rounded-full py-1 px-3 font-bold mr-2' type='button'>Sci - Fi</button>
+                <button className='bg-gray-400 rounded-full py-1 px-3 font-bold mr-2' type='button'>Adventure</button>
+                <button className='bg-gray-400 rounded-full py-1 px-3 font-bold mr-2' type='button'>Slice Of Life</button>
+              </div>
+            </div>
+        </div>
         <div className='flex flex-wrap mx-auto justify-between items-center'>
                 {list.map((list) => {
                 return (
                 <div>
-                    <div className="card max-w-[400px] mb-5 bg-base-100 shadow-xl mx-2 image-full h-[500px]">
+                    <div className="card max-w-[400px] mb-5 bg-base-100 shadow-xl mx-1 image-full h-[500px]">
                         <figure><img className='object-cover w-[400px] h-[600px]' src={list.image} alt="Shoes" /></figure>
                         <div className="card-body">
                             <h2 className="card-title">{list.nama}</h2>
